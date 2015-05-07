9a75f398c190fb0aa47fcab92931944b5c2f9c20
fpi-ii <github.me_working@spamgourmet.com>
2015-05-07 22:19:44 +0200

    Added plot 2

--- plot2.R
+++ plot2.R
@@ -1,4 +1,3 @@
-
 dfile <- "./data/household_power_consumption.txt";
 # read the file
 dfall <- read.table(dfile, sep=";", header=TRUE, stringsAsFactors=FALSE);
@@ -8,9 +7,11 @@ df <- dfall[(dfall$Date == "1/2/2007" | dfall$Date == "2/2/2007"),];
 
 # read.csv read (almost) every column in the data file is of type character 
 # now transform it to numeric as needed
+df <- transform(df, Date = as.Date(Date, "%d/%m/%Y"));
+df <- transform(df, Time = strftime(strptime(Time, "%H:%M:%S"),"%H:%M:%S"));
 df <- transform(df, Global_active_power = as.numeric(Global_active_power));
 
-# create the first plot, save it into a png file, close the png graph device
-hist(df$Global_active_power, col='red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)');
-dev.copy(png, file = 'plot1.png', height=480, width=480)
+datetime <- strptime(paste(df$Date, df$Time, sep=" "), "%Y-%m-%d %H:%M:%S");
+plot(datetime, df$Global_active_power, type = 'l', ylab = 'Global Active Power (kilowatts)', xlab='');
+dev.copy(png, file = 'plot2.png', height=480, width=480)
 dev.off();
